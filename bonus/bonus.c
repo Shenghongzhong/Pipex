14fa477ed2559e3928bc0edc72ed057774db6012
szhong <szhong@student.42london.com>
2024-05-01 17:19:53 +0100

    for the record

--- bonus/bonus.c
+++ bonus/bonus.c
@@ -1,56 +1,58 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   bonus.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: szhong <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/29 18:15:29 by szhong            #+#    #+#             */
-/*   Updated: 2024/04/30 18:02:04 by szhong           ###   ########.fr       */
+/*   Updated: 2024/05/01 17:18:52 by szhong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 #include <sys/wait.h>
 #include <stdio.h>
+#include "bonus.h"
 
-
-void	child_proc(int *p_fd, char **argv)
+void	child_process(int *p_fd, char *input, char *arg, char *env[])
 {
 	int	infile;
 
-	infile = open(argv[1], O_RDONLY, 0777);
+	infile = open(input, O_RDONLY, 0777);
 	if (infile == -1)
 	{
-		perror("open() Error");
-		exit(EXIT_FAILURE);
+		ft_putstr_fd("no such file or directory: ", 2);
+		ft_putendl_fd(input, 2);
 	}
 	if (dup2(infile, STDIN_FILENO) == -1)
 	{
-		ft_putstr_fd("no such file:", 2);
-		ft_putendl_fd(argv[1], 2);
-		perror("dup2(infile, STDIN_FILENO)\nError");
+		perror("dup2(infile,STDIN_FILENO)\nError");
+		exit(EXIT_FAILURE);
 	}
 	if (dup2(p_fd[1], STDOUT_FILENO) == -1)
+	{
 		perror("dup2(p_fd[1], STDOUT_FILENO)\nError");
-	exec_cmds();
+		exit(EXIT_FAILURE);
+	}
+	exec_cmds(arg, env);
 	exit(EXIT_SUCCESS);
 }
 
-void	parent_proc(int *p_fd, char **argv)
+void	parent_process(int *p_fd, char *output, char *arg, char *env[])
 {
 	int	outfile;
 
-	outfile = open(argv[], O_RWONLY | O_CREAT | O_TRUNC, 0777);
+	outfile = open(output, O_RDWR | O_CREAT | O_TRUNC, 0777);
 	if (outfile == -1)
 		perror("open() Error");
 	if (dup2(p_fd[0], STDIN_FILENO) == -1)
 		perror("dup2(p_fd[0], STDIN_FILENO)\nError");
 	if (dup2(outfile, STDOUT_FILENO) == -1)
 		perror("dup2(outfile, STDOUT_FILENO)\nError");
-	exec_cmd();
+	exec_cmds(arg, env);
 }
 
-int	main(int argc, char *argv[])
+int	main(int argc, char *argv[], char **env)
 {
 	pid_t	pid;
 	int	p_fd[2];
@@ -58,18 +60,11 @@ int	main(int argc, char *argv[])
 	int	i;
 	int	j;
 
-	if (argc < 0)
-		ft_printf("error\n");//error_handler(-1);
-	num_cmds = 0;
-	j = -1;
-	while (*argv)
-	{
-		num_cmds++;
-		argv++;
-	}
-	num_cmds -= 3;
-	ft_printf("cmds are %d\n", num_cmds);
+	if (argc < 5)
+		error_handler(-1);
+	num_cmds = argc - 3;
 	i = -1;
+	j = 2;
 	if (pipe(p_fd) == -1)
 	{
 		perror("Pipe Creation Failure");
@@ -86,9 +81,11 @@ int	main(int argc, char *argv[])
 		else if (pid > 0)
 		{
 			wait(NULL);
-			parent_doc(p_fd[0], argv);
+			parent_process(p_fd, argv[1], argv[j + 1], env);
 		}
 		else 
-			child_proc(p_fd[1], argv);
+			child_process(p_fd, argv[argc - 1], argv[j], env);
+		if (j + 1 == num_cmds)
+			break ;
 	}
 }
